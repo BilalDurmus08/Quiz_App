8465d9b7f43a4ea1eaa1a33bc8e731923acddb4c
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-04-06 15:11:03 +0300

    shuffled method added

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,6 +1,12 @@
-class QuizQuestion{
+class QuizQuestion {
   const QuizQuestion(this.text, this.answers);
- 
+
   final String text;
-  final List<String> answers; 
+  final List<String> answers;
+
+  List<String> shuffledQuizQuestion() {
+    List<String> shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
