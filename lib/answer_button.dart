3a47cb40bb6d818e0a0896fc085439d4f1868a35
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-04-06 15:11:57 +0300

    google fonts added

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AnswerButton extends StatelessWidget {
   const AnswerButton(
@@ -11,10 +12,20 @@ class AnswerButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onTap,
-      child: Text(answerText),
       style: ElevatedButton.styleFrom(
-        backgroundColor: Color.fromARGB(255, 85, 4, 100),
-        foregroundColor: Colors.white,
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+          backgroundColor: const Color.fromARGB(255, 85, 4, 100),
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.archivo(
+          color: const Color.fromARGB(255, 213, 162, 222),
+          fontSize: 14,
+          fontWeight: FontWeight.w500
+        ),
       ),
     );
   }
